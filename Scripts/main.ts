8ab69d1db339a871f27ec607e2db1e2acf2ca941
Karina Muñoz <k.munoz1@uniandes.edu.com>
2025-04-06 23:33:07 -0500

    final

--- Scripts/main.ts
+++ Scripts/main.ts
@@ -4,6 +4,7 @@ import { series } from './data.js';
 
 let tablaBody: HTMLElement = document.getElementById('series-table-body')!;
 const promedioElemento: HTMLElement = document.getElementById("seasons-average")!;
+const cartaElemento: HTMLElement = document.getElementById("detalle-serie")!;
 
 
 renderSeriesInTable(series);
@@ -13,10 +14,11 @@ function renderSeriesInTable(series: Serie[]): void {
   series.forEach(c => {
     let elemento = document.createElement("tr");
     elemento.innerHTML = `<td><strong>${c.number}</strong></td>
-                           <td> <a href= ${c.link}>${c.name}</a> </td>
+                           <td> ${c.name}</a> </td>
                            <td>${c.channel}</td>
                            <td>${c.seasons}</td>`;
     tablaBody.appendChild(elemento);
+    elemento.addEventListener("click", () => MostrarDetalleSerie(c));
   });
 }
 
@@ -27,4 +29,17 @@ function promedio(series: Serie[]): number {
     return temporadas/nseries;
 
   }
+  
+  function MostrarDetalleSerie(c: Serie): void {
+      cartaElemento.innerHTML = `
+        <div class="card">
+          <img src="${c.imagen}" class="card-img-top" alt="${c.name}">
+          <div class="card-body">
+            <h5 class="card-title">${c.name}</h5>
+            <p class="card-text">${c.descripcion}</p>
+            <a href="${c.link}" target="_blank"> ${c.link}</a>
+          </div>
+        </div>
+      `;
+    }    
 
--- Scripts/scripts/main.js
+++ Scripts/scripts/main.js
@@ -1,16 +1,18 @@
 import { series } from './data.js';
 let tablaBody = document.getElementById('series-table-body');
 const promedioElemento = document.getElementById("seasons-average");
+const cartaElemento = document.getElementById("detalle-serie");
 renderSeriesInTable(series);
 promedioElemento.innerHTML = `Seasons average: ${promedio(series)}`;
 function renderSeriesInTable(series) {
     series.forEach(c => {
         let elemento = document.createElement("tr");
         elemento.innerHTML = `<td><strong>${c.number}</strong></td>
-                           <td> <a href= ${c.link}>${c.name}</a> </td>
+                           <td> ${c.name}</a> </td>
                            <td>${c.channel}</td>
                            <td>${c.seasons}</td>`;
         tablaBody.appendChild(elemento);
+        elemento.addEventListener("click", () => MostrarDetalleSerie(c));
     });
 }
 function promedio(series) {
@@ -18,4 +20,16 @@ function promedio(series) {
     let nseries = series.length;
     series.forEach((serie) => temporadas = temporadas + serie.seasons);
     return temporadas / nseries;
+}
+function MostrarDetalleSerie(c) {
+    cartaElemento.innerHTML = `
+        <div class="card">
+          <img src="${c.imagen}" class="card-img-top" alt="${c.name}">
+          <div class="card-body">
+            <h5 class="card-title">${c.name}</h5>
+            <p class="card-text">${c.descripcion}</p>
+            <a href="${c.link}" target="_blank"> ${c.link}</a>
+          </div>
+        </div>
+      `;
 }
